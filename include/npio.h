1062211259dd1d7f1a2459e0e40f14b99c68b0b3
Erik Wernersson <erik.wernersson@gmail.com>
2025-01-27 09:45:17 +0100

    renamed functions

--- include/npio.h
+++ include/npio.h
@@ -2,16 +2,18 @@
 
 /* A library to read/write numpy .npy-files
  * see npio_cli.c for example usage.
-*/
+ *
+ * web: https://www.github.com/elgw/npio
+ */
 
 #include <stdint.h>
 #include <stdio.h>
 
 #define NPIO_VERSION_MAJOR "0"
 #define NPIO_VERSION_MINOR "0"
-#define NPIO_VERSION_PATCH "3"
-#define NPIO_version NPIO_VERSION_MAJOR "."  \
-    NPIO_VERSION_MINOR "."                        \
+#define NPIO_VERSION_PATCH "4"
+#define NPIO_version NPIO_VERSION_MAJOR "."     \
+    NPIO_VERSION_MINOR "."                      \
     NPIO_VERSION_PATCH
 
 /* This is what is returned from npio_load */
@@ -47,53 +49,53 @@ npio_t * npio_load(const char * filename);
 
 
 /** Save a double array to an npy file */
-int npio_save_double(const char * filename,
-              const int ndim, const int * shape,
-              const double * data);
+int npio_save_f64(const char * filename,
+                  const int ndim, const int * shape,
+                  const double * data);
 
-int npio_save_float(const char * filename,
-                     const int ndim, const int * shape,
-                     const float * data);
+int npio_save_f32(const char * filename,
+                  const int ndim, const int * shape,
+                  const float * data);
 
-int npio_save_int8_t(const char * filename,
-                     const int ndim, const int * shape,
-                     const int8_t * data);
+int npio_save_i8(const char * filename,
+                 const int ndim, const int * shape,
+                 const int8_t * data);
 
-int npio_save_int16_t(const char * filename,
-                     const int ndim, const int * shape,
-                     const int16_t * data);
+int npio_save_i16(const char * filename,
+                  const int ndim, const int * shape,
+                  const int16_t * data);
 
-int npio_save_int32_t(const char * filename,
-                     const int ndim, const int * shape,
-                     const int32_t * data);
+int npio_save_i32(const char * filename,
+                  const int ndim, const int * shape,
+                  const int32_t * data);
 
-int npio_save_int64_t(const char * filename,
-                     const int ndim, const int * shape,
-                     const int64_t * data);
+int npio_save_i64(const char * filename,
+                  const int ndim, const int * shape,
+                  const int64_t * data);
 
-int npio_save_uint8_t(const char * filename,
-                    const int ndim, const int * shape,
-                    const uint8_t * data);
+int npio_save_u8(const char * filename,
+                 const int ndim, const int * shape,
+                 const uint8_t * data);
 
-int npio_save_uint16_t(const char * filename,
-                      const int ndim, const int * shape,
-                      const uint16_t * data);
+int npio_save_u16(const char * filename,
+                  const int ndim, const int * shape,
+                  const uint16_t * data);
 
-int npio_save_uint32_t(const char * filename,
-                       const int ndim, const int * shape,
-                       const uint32_t * data);
+int npio_save_u32(const char * filename,
+                  const int ndim, const int * shape,
+                  const uint32_t * data);
 
-int npio_save_uint64_t(const char * filename,
-                       const int ndim, const int * shape,
-                       const uint64_t * data);
+int npio_save_u64(const char * filename,
+                  const int ndim, const int * shape,
+                  const uint64_t * data);
 
 
 
 /** @brief Print some info about the npio_t object
  *
-*/
+ */
 void npio_print(FILE *, const npio_t * np);
 
 /**  Free an npio_t object
-*/
+ */
 void npio_free(npio_t * np);
